b5d00e579264a68119d180ce6699941eb9eee568
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-20 13:22:51 -0300

    update

--- ft_printf.h
+++ ft_printf.h
@@ -22,4 +22,5 @@ int		ft_printf(const char * format, ...);
 int		print_string(const char *format, va_list ap, t_option *option);
 void	check_options(const char *format, va_list ap, t_option *option);
 
+
 #endif
