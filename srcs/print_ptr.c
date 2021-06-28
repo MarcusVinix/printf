edf5d218efc4399dd2de12b6eb748f7054f5bc9e
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-28 16:05:10 -0300

    update

--- srcs/print_ptr.c
+++ srcs/print_ptr.c
@@ -6,14 +6,54 @@
 /*   By: mavinici <mavinici@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 18:14:24 by mavinici          #+#    #+#             */
-/*   Updated: 2021/06/25 18:17:16 by mavinici         ###   ########.fr       */
+/*   Updated: 2021/06/28 15:26:48 by mavinici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+void	ft_putnbr_base_long_fd(unsigned long int num, char *base, int fd)
+{
+	size_t len_base;
+
+	len_base = ft_strlen(base);
+	if (num / len_base > 0)
+		ft_putnbr_base_long_fd(num / len_base, base, fd);
+	ft_putchar_fd(base[num % len_base] , fd);
+}
+
+static int	count_digits(unsigned long int num, int base, t_option *option)
+{
+	int	count;
+
+	count = 0;
+	if (num == 0)
+		return (1);
+	while (num >= 1)
+	{
+		num /= base;
+		count++;
+	}
+	count += option->num_n;
+	return (count);
+}
+
 void	print_ptr(va_list ap, t_option *option)
 {
-	(void) ap;
-	(void) option;
+	unsigned long int	number;
+	int					digits;
+
+	number = va_arg(ap, unsigned long int);
+	digits = count_digits(number, 16, option) + 2;
+	if (option->width > digits)
+		option->space = option->width - digits;
+	option->count += option->space + digits;
+	if (!option->flag_minus)
+		while (option->space-- > 0)
+			ft_putchar_fd(' ', 1);
+	ft_putstr_fd("0x", 1);
+	ft_putnbr_base_long_fd(number, B_HEXA_x, 1);
+	if (option->flag_minus)
+		while (option->space-- > 0)
+			ft_putchar_fd(' ', 1);
 }
