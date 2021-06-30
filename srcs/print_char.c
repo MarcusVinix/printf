c9932ee7f5013a4523b3ee9986f3c9126a3848a3
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-30 18:13:29 -0300

    update

--- srcs/print_char.c
+++ srcs/print_char.c
@@ -6,7 +6,7 @@
 /*   By: mavinici <mavinici@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 11:44:13 by mavinici          #+#    #+#             */
-/*   Updated: 2021/06/25 18:14:36 by mavinici         ###   ########.fr       */
+/*   Updated: 2021/06/30 18:13:06 by mavinici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,25 @@
 void	print_char(va_list ap, t_option *option)
 {
 	char	c;
-	int		space;
 
 	c = va_arg(ap, int);
-	if (option->width > 1)
-		space = option->width - 1;
+	if (option->width > 1 && option->flag_zero)
+		option->zero = option->width - 1;
 	else
-		space = 0;
-	option->count += space + 1;
+		option->zero = 0;
+	if (option->width > 1 && !option->zero)
+		option->space = option->width - 1;
+	else
+		option->space = 0;
+	option->count += option->space + option->zero 1;
 	if (!option->flag_minus)
-		while (space-- > 0)
+		while (option->space-- > 0)
 			ft_putchar_fd(' ', 1);
+	if (option->flag_zero)
+		while (option->zero-- > 0)
+			ft_putchar_fd('0', 1);
 	ft_putchar_fd(c, 1);
 	if (option->flag_minus)
-		while (space-- > 0)
+		while (option->space-- > 0)
 			ft_putchar_fd(' ', 1);
 }
